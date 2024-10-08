e41541f6f07bbb517ed3ec51b7b8bbffbef0632b
Shivam <129513256+ShibuThakur0776@users.noreply.github.com>
2024-10-08 20:46:12 +0530

    added vegetable-mart-app

--- Angular/TaskManager/src/app/dashboard/dashboard.component.ts
+++ Angular/TaskManager/src/app/dashboard/dashboard.component.ts
@@ -15,6 +15,11 @@ export class DashboardComponent implements OnInit{
   ProjectCost: number=0;
   CurrentExpenditure :number=0;
   AvailableFunds: number=0;
+  Clients:string[] = [];
+  Projects:string[] = [];
+  Years:number[] = [];
+  TeamMemberSummary: any[] = [];
+  TeamMembers: any[] = [];
 
   ngOnInit(): void {
     this.Designation = "Team Leader";
@@ -26,5 +31,44 @@ export class DashboardComponent implements OnInit{
     this.ProjectCost = 2113507;
     this.CurrentExpenditure = 96788;
     this.AvailableFunds = 52536;
+    this.Clients = ["ABC Infotech Ltd","DEF Soft Solutions","GHI Industries"];
+    this.Projects = ["Project A","Project B","Project C","Project D"];
+    
+    for(var i=2019; i>=2010; i--){ 
+      this.Years.push(i);
+    }
+   
+    this.TeamMemberSummary = [
+      {Region:"East",TeamMembersCount: 20, TemporarilyUnavailableMembers:4},
+      {Region:"West",TeamMembersCount: 15, TemporarilyUnavailableMembers:8},
+      {Region:"East",TeamMembersCount: 17, TemporarilyUnavailableMembers:1},
+      {Region:"East",TeamMembersCount: 15, TemporarilyUnavailableMembers:6}
+    ]
+
+    this.TeamMembers = [{Region:"East",Member:[
+        {ID: 1, Name: "Ford", Status: "Available"},
+        {ID: 2, Name: "Miller", Status: "Available"},
+        {ID: 3, Name: "Jones", Status: "Busy"},
+        {ID: 4, Name:"James", Status:"Busy"}],
+      },
+      {Region:"West",Member:[
+        {ID: 5, Name: "Sunil", Status: "Available"},
+        {ID: 6, Name: "Indu", Status: "Available"},
+        {ID: 7, Name: "Anuj", Status: "Busy"},
+        {ID: 8, Name:"Sonu", Status:"Busy"}
+      ],},
+      {Region:"North",Member:[
+        {ID: 9, Name: "Abhisekh", Status: "Available"},
+        {ID: 10, Name: "Sunny", Status: "Available"},
+        {ID: 11, Name: "Rattan", Status: "Busy"},
+        {ID: 12, Name:"Diksha", Status:"Busy"}
+      ],},
+      {Region:"South",Member:[
+        {ID: 13, Name: "Niranjan", Status: "Available"},
+        {ID: 14, Name: "Kashif", Status: "Available"},
+        {ID: 15, Name: "Harshit", Status: "Busy"},
+        {ID: 16, Name:"Arjun", Status:"Busy"}
+      ],},
+  ]
   }
 }
